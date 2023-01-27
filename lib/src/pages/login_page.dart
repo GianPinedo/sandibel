0f5871f26c7d76783c292b13a84aceb86277385d
Gian Pinedo <36744525+GianPinedo@users.noreply.github.com>
2023-01-27 15:44:25 -0500

    second

---
+++ lib/src/pages/login_page.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+//stateful state
+class LoginPage extends StatefulWidget {
+  static String id = "login_page";
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+//state
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          child: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Image.asset(
+                'images/logosandi.png',
+                height: 200.0,
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              _userTextField(),
+              SizedBox(
+                height: 20.0,
+              ),
+              _passwordTextField(),
+              SizedBox(
+                height: 20.0,
+              ),
+              _loginButton(),
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _userTextField() {
+    return StreamBuilder(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: TextField(
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            icon: Icon(Icons.email),
+            hintText: 'abc@gmail.com',
+            labelText: 'Correo',
+          ),
+          onChanged: (value) {},
+        ),
+      );
+    });
+  }
+
+  Widget _passwordTextField() {
+    return StreamBuilder(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: TextField(
+          obscureText: true,
+          decoration: InputDecoration(
+            icon: Icon(Icons.lock),
+            hintText: '********',
+            labelText: 'Contraseña',
+          ),
+          onChanged: (value) {},
+        ),
+      );
+    });
+  }
+
+  Widget _loginButton() {
+    return StreamBuilder(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return ElevatedButton(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+          child: Text('Iniciar Sesión'),
+        ),
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          elevation: 0.0,
+          primary: Colors.blue,
+          onPrimary: Colors.white,
+        ),
+        onPressed: () {},
+      );
+    });
+  }
+}
